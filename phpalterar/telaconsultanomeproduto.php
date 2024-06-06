50de8cda6d2c3dfc8e5c7201eefab493492aa5e8
Julia <117773552+Juliadiasfs@users.noreply.github.com>
2024-06-06 16:15:12 -0300

    Add files via upload

---
+++ phpalterar/telaconsultanomeproduto.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <form action="telaexibirdadosprodutos.php" method="POST">
+      Digite o nome do Produto: <br>
+      <input type="text" name="cxpesquisaproduto"/>
+      <input type="submit" value="Pesquisar">
+     </form>
+</body>
+</html>
